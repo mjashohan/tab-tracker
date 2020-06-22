718fd269dc6ff1248dbde4d37ebad19ea273eafa
Md Jahan Ali Shohan <mjashohan@gmail.com>
2020-06-22 22:32:13 +0200

    Setting up the registration page

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-tabs': 'off',
+    'indent': 'off'
+  }
+}
