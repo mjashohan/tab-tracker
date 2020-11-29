5e040d6949b38171a3683287ca7d6bfa1cb85405
Shohan <mjashohan@gmail.com>
2020-11-29 17:02:37 +0100

    The backend

---
+++ client/src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <h1>Here is a list of Proxy IPs:</h1>
+  <div v-for="ips in IP" :key="ips">
+      {{ips.IPAddress}}
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'home',
+  data () {
+    return {
+      IP: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:8081/getIP')
+      .then((response) => {
+        this.IP = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
+
+</script>
+
+<style>
+
+</style>
